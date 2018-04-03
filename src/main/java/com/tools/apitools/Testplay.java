2fb430d46bf56da8869733419b8ebce2f6eaabb0
Administrator <Administrator@DEEP-1802110216>
2018-04-03 21:33:46 +0800

    test

---
+++ src/main/java/com/tools/apitools/Testplay.java
@@ -0,0 +1,13 @@
+package com.tools.apitools;
+
+import java.util.Map;
+
+import misun_web.Interface.httpInterface;
+
+public class Testplay extends httpInterface{
+
+	public Testplay(String URL, Map<String, Object> MAP) {
+		super(URL, MAP);
+	}
+	
+}
--- src/main/java/com/tools/steps/HttpDemo.java
+++ src/main/java/com/tools/steps/HttpDemo.java
@@ -3,25 +3,22 @@ package com.tools.steps;
 import java.util.Map;
 
 import com.alibaba.fastjson.JSONObject;
+import com.tools.apitools.Testplay;
 
 import cucumber.api.java.zh_cn.当;
 import cucumber.api.java.zh_cn.那么;
-import misun_web.Interface.httpInterface;
 import misun_web.Interface.responseInterface;
 
-public class HttpDemo extends httpInterface{
+public class HttpDemo{
 	
 	private String  result;
 	
-	public HttpDemo(String URL, Map<String, Object> MAP) {
-		super(URL, MAP);
-		// TODO Auto-generated constructor stub
-	}   
+	
 	@当("^我发送一个请求 : \"([^\"]*)\" 参数为$")
 	public void postRequest(String url,String json) throws Throwable {
         JSONObject jObject = JSONObject.parseObject(json);
         Map<String , Object> map = jObject.getInnerMap();
-        result = new HttpDemo(url, map).getResult();
+        result = new Testplay(url, map).getResult();
     }
 	@那么 ("^拿到的返回结果为:$" )
 	public void getResults() {
