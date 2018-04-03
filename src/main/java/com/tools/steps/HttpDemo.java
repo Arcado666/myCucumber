0e033ce03b8a16c882df863fa6e1e0d8583d3ad7
Administrator <Administrator@DEEP-1802110216>
2018-04-03 21:18:53 +0800

    update

--- src/main/java/com/tools/steps/HttpDemo.java
+++ src/main/java/com/tools/steps/HttpDemo.java
@@ -16,14 +16,14 @@ public class HttpDemo extends httpInterface{
 	public HttpDemo(String URL, Map<String, Object> MAP) {
 		super(URL, MAP);
 		// TODO Auto-generated constructor stub
-	}
-	@当("^我发送一个请求 : \"(.*?)\" 参数为 \"(.*?)\" $")
+	}  
+	@当("^我发送一个请求 : \"(.*?)\" 参数为 $")
 	public void postRequest(String url,String json) throws Throwable {
         JSONObject jObject = JSONObject.parseObject(json);
         Map<String , Object> map = jObject.getInnerMap();
         result = new HttpDemo(url, map).getResult();
     }
-	@那么 ("^拿到的返回结果为: $" )
+	@那么 ("^拿到的返回结果为:$" )
 	public void getResults() {
 		responseInterface rsi = new responseInterface(result);
 		System.out.println(rsi.getMsg());
