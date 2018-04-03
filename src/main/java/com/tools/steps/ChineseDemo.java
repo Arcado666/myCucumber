798a342de1de16b17d75b5bdbdae2d4655835b35
Administrator <Administrator@DEEP-1802110216>
2018-04-03 10:31:23 +0800

    update

--- src/main/java/com/tools/steps/ChineseDemo.java
+++ src/main/java/com/tools/steps/ChineseDemo.java
@@ -45,43 +45,43 @@ public class ChineseDemo {
 
 	}
 
-	@假如("^:给定了如下的动物:\"([^\"]*)\"$")
-	public void 给定了如下的动物(@Delimiter(",") List<String> arg1) throws Throwable {
-		if (null != arg1 && !arg1.isEmpty()) {
-			System.out.println("list长度为:" + arg1.size());
-			for (String str : arg1) {
-				System.out.println(str);
-			}
-		}
-	}
-
-	@假如("^:给定了如下的动物:$")
-	public void 给定了如下的动物(DataTable animals) throws Throwable {
-		List<String> animalsList = animals.asList(String.class);
-		if (null != animalsList && !animalsList.isEmpty()) {
-			System.out.println("list长度为:" + animalsList.size());
-			for (String str : animalsList) {
-				System.out.println("DataTable:" + str);
-			}
-		}
-	}
-
-	@假如("^:给定的日期为\"([^\"]*)\"$")
-	public void 给定的日期为(@Format("yyyy-MM-dd'T'HH:mm:ss") Date date) throws Throwable {
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		System.out.println(sdf.format(date));
-	}
-
-	@假如("^:给定的日期为\"([^\"]*)\"$")
-	public void 给定的日期为(@Format("yyyy-MM-dd'T'HH:mm:ss") Calendar cal) throws Throwable {
-		cal.add(Calendar.YEAR, 1);
-		System.out.println(cal.get(Calendar.YEAR));
-	}
-
-	@假如("^:给定的user信息如下$")
-	public void 给定的user信息如下(@Transpose List<String> user) throws Throwable {
-		for (String u : user) {
-			System.out.println(u.toString());
-		}
-	}
+//	@假如("^:给定了如下的动物:\"([^\"]*)\"$")
+//	public void 给定了如下的动物(@Delimiter(",") List<String> arg1) throws Throwable {
+//		if (null != arg1 && !arg1.isEmpty()) {
+//			System.out.println("list长度为:" + arg1.size());
+//			for (String str : arg1) {
+//				System.out.println(str);
+//			}
+//		}
+//	}
+//
+//	@假如("^:给定了如下的动物:$")
+//	public void 给定了如下的动物(DataTable animals) throws Throwable {
+//		List<String> animalsList = animals.asList(String.class);
+//		if (null != animalsList && !animalsList.isEmpty()) {
+//			System.out.println("list长度为:" + animalsList.size());
+//			for (String str : animalsList) {
+//				System.out.println("DataTable:" + str);
+//			}
+//		}
+//	}
+//
+//	@假如("^:给定的日期为\"([^\"]*)\"$")
+//	public void 给定的日期为(@Format("yyyy-MM-dd'T'HH:mm:ss") Date date) throws Throwable {
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//		System.out.println(sdf.format(date));
+//	}
+//
+//	@假如("^:给定的日期为\"([^\"]*)\"$")
+//	public void 给定的日期为(@Format("yyyy-MM-dd'T'HH:mm:ss") Calendar cal) throws Throwable {
+//		cal.add(Calendar.YEAR, 1);
+//		System.out.println(cal.get(Calendar.YEAR));
+//	}
+//
+//	@假如("^:给定的user信息如下$")
+//	public void 给定的user信息如下(@Transpose List<String> user) throws Throwable {
+//		for (String u : user) {
+//			System.out.println(u.toString());
+//		}
+//	}
 }
